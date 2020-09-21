82ee30a3cde367d7c2b6f6798a80775f832f37b3
Santiago <santiago_figueroa_@hotmail.com>
2020-09-21 18:30:17 +0100

    Rot13 added function

---
+++ Scripts/Rot.js
@@ -0,0 +1,27 @@
+class Rot { 
+    resultArray = new Array();  
+    numSytrings = 0 ;
+    
+// 
+rot13(str){
+    
+    // works now counting chars
+    for(var s in str){
+        this.numSytrings ++;
+        //console.log(s1.charAt(s));
+        var tmp = str.charAt(s);
+        // Checks when ever is a lower and that is not empty.
+        if(tmp == tmp.toLowerCase() && tmp != " "){
+          //  console.log(tmp);
+          // 
+          var char = codeToChar((tmp.charCodeAt(0) - 'a'.charCodeAt(0)+13) ) 
+          //
+          this.resultArray.push(char);
+        }
+    
+    }
+    return this.resultArray; 
+}
+
+
+}
